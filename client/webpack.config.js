191b9877dce50f1c9de87e657c67ecf488c84765
NotANewt <meegafauna@gmail.com>
2022-04-30 16:15:16 -0500

    added plugins to create html file, generate service worker, and create manifest.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,30 +1,54 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { InjectManifest } = require("workbox-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      
+      // create html file for dist
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Just Another Text Editor",
+      }),
+      // generate service worker
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      // create manifest.json
+      new WebpackPwaManifest({
+        name: "Just Another Text Editor",
+        description: "A text editor.",
+        background_color: "black",
+        theme_color: "hotpink",
+        inject: true,
+        fingerprints: false,
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
-      rules: [
-        
-      ],
+      rules: [],
     },
   };
 };
